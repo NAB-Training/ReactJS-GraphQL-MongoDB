997263464a907df14181117f3dd708c1a4fbbd6f
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-04-16 05:31:21 +0700

    get all students with UI

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,15 @@ import StudentTable from "./components/students_table";
 import FormStudent from './components/form_student';
 import FormSchool from './components/form_school';
 import FormTeacher from './components/form_teacher';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+	uri: 'http://localhost:8000/graphql',
+	cache: new InMemoryCache()
+})
 function App() {
     return (
+        <ApolloProvider client={client}>
         <React.Fragment>
             <CssBaseline />
             <Box sx={{ bgcolor: 'black', backgroundColor: '#cfe8fc', height: 'lg' }}>
@@ -19,6 +26,7 @@ function App() {
                 </Container>
             </Box>
         </React.Fragment>
+        </ApolloProvider>
     );
 }
 
